1e395d14a050e3d9fff37d9ab28e9bce040978f5
hamidseptian <hamidseptian@gmail.com>
2026-01-06 14:05:12 +0700

    dockerfile part 6

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,11 @@ date_default_timezone_set('Asia/Jakarta');
 |
 */
 //$config['base_url'] = 'https://simbangda.sumbarprov.go.id/2020/';
-$config['base_url'] = $_ENV['BASE_URL'];
+// $config['base_url'] = $_ENV['BASE_URL'];
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "https");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+
 
 //$config['base_url'] = 'http://localhost/simbangda2020/';
 /*
