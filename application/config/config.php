c3716f86eeed9499835ad7f5278b93daa8909105
hamidseptian <hamidseptian@gmail.com>
2026-01-08 15:54:57 +0700

    penambahan proxy view evidence R01

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('Asia/Jakarta');
 */
 //$config['base_url'] = 'https://simbangda.sumbarprov.go.id/2020/';
 // $config['base_url'] = $_ENV['BASE_URL'];
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "https");
 $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
