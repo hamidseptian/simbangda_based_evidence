72703c923802e608432616e6c19771a44c20c4ff
hamidseptian <hamidseptian@gmail.com>
2026-01-14 12:38:53 +0700

    perbaiki detail export import

--- application/modules/export_import/views/data_apbd/kelola_import_opd.php
+++ application/modules/export_import/views/data_apbd/kelola_import_opd.php
@@ -846,8 +846,9 @@ $data_insert_all = [
                                                 <div class="col-md-12">
                                                     <div class="btn-group btn-block">
                                                         
-                                            <button class=" btn btn-outline-info" type="button" onclick="import_all_data_apbd()">Import Data APBD (Pilih Data APBD, Pagu Sub Kegiatan, Sumber dana Sub Kegiatan)</button>
-                                            <a class="btn btn-outline-info" href="<?php echo base_url('data_apbd/setting') ?>">Pilih Data APBD (Manual Input)</a>
+                                           <!--  <button class=" btn btn-outline-info" type="button" onclick="import_all_data_apbd()">Import Data APBD (Pilih Data APBD, Pagu Sub Kegiatan, Sumber dana Sub Kegiatan)</button>
+                                            <a class="btn btn-outline-info" href="<?php echo base_url('data_apbd/setting') ?>">Pilih Data APBD (Manual Input)</a> -->
+                                            <button class="btn btn-block btn-info" onclick="alert('coming soon, sedang dalam pengembangan')" type="button">Import akan segera di aktifkan</button>
                                                     
                                                     </div>
                                                 </div>
